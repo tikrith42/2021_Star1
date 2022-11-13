ccbd06951700cca416c4a6a1c6dc1406838f808f
tikrith42 <70075582+tikrith42@users.noreply.github.com>
2022-11-12 18:42:54 -0800

    Add files via upload

---
+++ onestar.py
@@ -0,0 +1,61 @@
+
+# initialize variables
+a=0
+b=0
+inc=0  # count of increases
+dec=0   # count of decreases
+noch=0   #count of no changes
+# measurement=0  #troubleshooting variable
+
+# determine how long the file is
+num_lines = sum(1 for _ in open('/Users/Tik/Downloads/input'))
+print (num_lines)
+
+
+f=open('/Users/Tik/Downloads/input','r')
+a=(f.readline())
+
+#for i in range(num_lines):
+
+while a!='':
+    b=(f.readline())
+
+
+# checks to see if reading lines and comparing correctly
+    #print(a)
+    #print(b)
+
+    if b=='':
+        break
+
+    elif b>a:
+        inc+=1
+        print('increase')
+    elif b<a:
+        dec+=1
+        print('decrease')
+    elif a==b:
+        noch+=1
+
+# troubleshooting count measurements
+    #measurement+=1
+    #print('measurement is :',measurement)
+
+    a=b
+    continue
+
+print ("increases equals ",inc)
+print ("Decreases equals ",dec)
+print ("No changes equals ",noch)
+
+
+
+
+
+
+# if a>b then:
+# increase+=1
+# else
+# decrease+=1
+
+    
